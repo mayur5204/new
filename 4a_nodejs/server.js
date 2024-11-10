904c5eeb4d08a614d68dfdbbae959e751f9adbae
mayur5204 <143496615+mayur5204@users.noreply.github.com>
2024-11-10 11:00:01 +0000

    xcbxc

--- 4a_nodejs/server.js
+++ 4a_nodejs/server.js
@@ -3,7 +3,6 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
