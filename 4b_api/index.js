647ec9202924ace92ba5a828ea636b9a0bfeed53
mayur5204 <mayur_patil_aiml@moderncoe.edu.in>
2024-11-10 17:40:55 +0530

    4b_api

--- 4b_api/index.js
+++ 4b_api/index.js
@@ -1,14 +1,37 @@
 const dbConnect = require('./mongodb');
 const express = require('express');
 const app = express();
+app.use(express.json());
 
 //get API
 
-app.get('/', async (req, res) => {
+app.get('/getData', async (req, res) => {
     let result = await dbConnect();
 
     result = await result.find().toArray();
     res.send(result);
+});
+
+//post API
+
+app.post('/postData', async (req, res) => {
+    let result = await dbConnect();
+    result = result.insertOne(req.body);
+    res.send("Data inserted successfully");
+});
+
+//put API
+app.put('/putData/:name', async (req, res) => {
+    let result = await dbConnect();
+    result = await result.updateOne({ name: req.params.name }, { $set: req.body });
+    res.send("Data updated successfully");
+});
+
+//delete API
+app.delete('/deleteData/:name', async (req, res) => {
+    let result = await dbConnect();
+    result = await result.deleteOne({ name: req.params.name });
+    res.send("Data deleted successfully");
 });
 
 const PORT = 4000;
